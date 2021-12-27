10fdef30e7ef7c6a4e200fb920b0008a27107c28
aybarsyazici <aybarsyazici@sabanciuniv.edu>
2021-12-27 13:34:16 +0300

    fix: text fix :)

--- frontend/src/components/navigationbar.tsx
+++ frontend/src/components/navigationbar.tsx
@@ -7,7 +7,7 @@ export const NavigationBar: FunctionComponent<NavbarProps> = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">CS 306 Project</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
